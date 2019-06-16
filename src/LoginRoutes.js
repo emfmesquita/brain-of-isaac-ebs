1f1726abb8e09fb9d62843ca1883567323f2423d
emfmesquita <edson.mesquita.ismart@gmail.com>
2019-06-16 03:06:51 -0300

    identation, removing unused

--- src/LoginRoutes.js
+++ src/LoginRoutes.js
@@ -1,9 +1,8 @@
-
 const fs = require("fs");
 const path = require("path");
-const handlebars = require('handlebars');
-const signature = require('cookie-signature');
-const querystring = require('querystring');
+const handlebars = require("handlebars");
+const signature = require("cookie-signature");
+const querystring = require("querystring");
 const axios = require("axios");
 require("dotenv").config();
 
@@ -12,26 +11,34 @@ const apiSecret = process.env.API_SECRET;
 
 const oauthRedirectUrl = process.env.OAUTH_REDIRECT_URL;
 const oauthCodeUrl = `https://id.twitch.tv/oauth2/authorize?client_id=${clientId}&response_type=code&force_verify=true&scope=&redirect_uri=${oauthRedirectUrl}`;
-const oauthTokenUrl = code => `https://id.twitch.tv/oauth2/token?client_id=${clientId}&client_secret=${apiSecret}&code=${code}&grant_type=authorization_code&redirect_uri=${oauthRedirectUrl}`;
+const oauthTokenUrl = code =>
+  `https://id.twitch.tv/oauth2/token?client_id=${clientId}&client_secret=${apiSecret}&code=${code}&grant_type=authorization_code&redirect_uri=${oauthRedirectUrl}`;
 const userUrl = "https://api.twitch.tv/helix/users";
 
-module.exports = (app) => {
-  const isLogged = (req) => {
+module.exports = app => {
+  const isLogged = req => {
     return req.session.userData && req.session.userData.id;
-  }
+  };
 
-  const loginPageTemplate = handlebars.compile(fs.readFileSync(path.resolve(__dirname, "login.html")) + "");
+  const loginPageTemplate = handlebars.compile(
+    fs.readFileSync(path.resolve(__dirname, "login.html")) + ""
+  );
 
   const sessionIdToClientPage = (req, port) => {
-    const sessionId = "s:" + signature.sign(req.sessionID, process.env.SESSION_SECRET);
+    const sessionId =
+      "s:" + signature.sign(req.sessionID, process.env.SESSION_SECRET);
     const userData = req.session.userData;
-    return loginPageTemplate({ ...userData, sessionId, clientUrl: `http://localhost:${port}` });
-  }
+    return loginPageTemplate({
+      ...userData,
+      sessionId,
+      clientUrl: `http://localhost:${port}`
+    });
+  };
 
   app.get("/login/:port", (req, res) => {
-    if(isLogged(req)){
+    if (isLogged(req)) {
       res.send(sessionIdToClientPage(req, req.params.port));
-    }else{
+    } else {
       res.redirect(oauthCodeUrl + `&state=${req.params.port}`);
     }
   });
@@ -44,16 +51,16 @@ module.exports = (app) => {
     let token;
 
     // gets twitch api token
-    try{
+    try {
       const tokenResponse = await axios.post(oauthTokenUrl(code));
       token = tokenResponse.data.access_token;
-    }catch(error){
+    } catch (error) {
       res.status(500).send(error);
       return;
     }
 
     // gets user info
-    try{
+    try {
       const userResponse = await axios.get(userUrl, {
         headers: {
           Authorization: "Bearer " + token
@@ -62,24 +69,24 @@ module.exports = (app) => {
       const userData = userResponse.data.data[0];
       req.session.userData = userData;
       res.send(sessionIdToClientPage(req, port));
-    }catch(error){
+    } catch (error) {
       res.status(500).send(error);
     }
   });
 
   app.get("/isloggedin", (req, res) => {
-    if(!isLogged(req)){
+    if (!isLogged(req)) {
       res.status(401).send("false");
-    } else{
+    } else {
       res.send("true");
     }
   });
 
   app.get("/logout", (req, res) => {
-    if(!isLogged(req)){
+    if (!isLogged(req)) {
       res.sendStatus(401);
-    } else{
-      req.session.destroy((err) => res.sendStatus(err ? 500 : 200));
+    } else {
+      req.session.destroy(err => res.sendStatus(err ? 500 : 200));
     }
   });
-}
+};
